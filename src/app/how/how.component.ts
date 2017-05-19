b4c726fc60d0aff1d485a2ba7b89684fca8a9ef3
diegofsza <diegofsza@gmail.com>
2017-05-19 11:54:52 -0300

    Added social media icons

--- src/app/how/how.component.ts
+++ src/app/how/how.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
+import {ScrollSpyParallaxOptions} from 'ngx-scrollspy/dist/plugin/parallax.directive';
 
 @Component({
     selector: 'app-how',
@@ -7,15 +8,14 @@ import {Component, ElementRef, OnInit} from '@angular/core';
 })
 export class HowComponent implements OnInit {
 
-    options = {
-        ratio: -.4,
-        initValue: -500
+    options: ScrollSpyParallaxOptions = {
+        ratio: -.4
     };
 
     constructor(private elementRef: ElementRef) {
     }
 
     ngOnInit() {
-        this.options.initValue = this.elementRef.nativeElement.offsetTop;
+        this.options.initValue = this.elementRef.nativeElement.offsetTop * 2;
     }
 }
