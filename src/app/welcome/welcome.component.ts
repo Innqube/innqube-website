2696d8c9cdf5e8130155401f41fcbec6d85c78a7
diegofsza <diegofsza@gmail.com>
2017-05-18 21:33:11 -0300

    Parallax

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -8,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class WelcomeComponent implements OnInit {
 
     options = {
-        ratio: -.3
+        ratio: -.4
     };
 
     constructor() {
