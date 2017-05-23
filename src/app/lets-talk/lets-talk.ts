dc5919a943b8df0c5e9ff78b7302f2f56f85d79e
diegofsza <diegofsza@gmail.com>
2017-05-23 11:31:36 -0300

    Modified contact form

--- src/app/lets-talk/lets-talk.ts
+++ src/app/lets-talk/lets-talk.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
-import {Http} from '@angular/http';
 
 @Component({
     selector: 'app-lets-talk',
@@ -11,7 +10,7 @@ export class LetsTalkComponent implements OnInit {
 
     form: FormGroup;
 
-    constructor(private formBuilder: FormBuilder, private http: Http) {
+    constructor(private formBuilder: FormBuilder) {
     }
 
     ngOnInit() {
@@ -20,20 +19,6 @@ export class LetsTalkComponent implements OnInit {
             mail: null,
             message: null
         });
-    }
-
-    sendMessage() {
-        const formData = new FormData();
-        formData.append('name', this.form.value.name);
-        formData.append('mail', this.form.value.mail);
-        formData.append('message', this.form.value.message);
-
-        this.http
-            .post(
-                'http://www.innqube.com/team/tomas-christie/',
-                formData
-            )
-            .subscribe();
     }
 
 }
